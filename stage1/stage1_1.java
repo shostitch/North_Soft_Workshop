2a12f2a747128b2c7bc15d624dcdce72ef9027b4
“shostitch“ <stitchsho0325@gmail.com>
2022-09-02 21:09:10 +0900

    1_5まで

--- stage1/stage1_1.java
+++ stage1/stage1_1.java
@@ -1,4 +1,9 @@
 package stage1;
+//練習問題 1 - 1
+
+// “Hello World”と表示するプログラムを作成しなさい。
+
+// ※ 	System.out.println()、System.out.print()、System.out.printf()などを使用する。
 
 public class stage1_1 {
   public static void main(String[] args){
